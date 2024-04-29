2ed62cf0bacc4ee9ac81b3ccff97dec8de44a61e
李法师 <liujiantao86@foxmail.com>
2024-04-29 16:22:16 +0800

    rpc address

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@ import stakeABI from "@/abi/StakeABI.json";
 import marsABI from "@/abi/MarsABI.json";
 // export const rpcUrl = "https://api.littlemami.io/";
 //test
-export const rpcUrl = "http://34.150.32.93:5556/";
+export const rpcUrl = "https://test-api.littlemami.io/";
+// export const rpcUrl = "http://34.150.32.93:5556/";
 
 export const contract = {
   1: {
