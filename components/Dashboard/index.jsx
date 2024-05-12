f198314589da7b39340acdf4b544fa8191194813
Web3TomCat <liujiantao86@foxmail.com>
2024-05-12 18:39:01 +0800

    --

--- components/Dashboard/index.jsx
+++ components/Dashboard/index.jsx
@@ -40,30 +40,45 @@ const LinkWrapper = styled.div`
 
 const Page2Wrapper = styled.div`
   height: 288px;
-  padding-left: 44px;
-  padding-top: 40px;
+  padding-left: 24px;
+  padding-top: 43px;
   border-top: 1.5px solid;
   border-image-source: linear-gradient(90deg, rgba(184, 68, 255, 0) 0%, rgba(182, 128, 250, 0.15) 52%, rgba(184, 68, 255, 0) 100%);
   border-image-slice: 1;
   border-image-outset: 0.8px;
-
-  // &:hover {
-  //   border-top: 4px solid;
-  //   border-image-source: linear-gradient(90deg, rgba(246, 98, 249, 1) 9.5%, rgba(65, 19, 112, 0) 100%);
-  //   border-image-outset: 2px;
-  //   & span {
-  //     background: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(222, 20, 255, 0) 100%), linear-gradient(166.65deg, rgba(246, 98, 250, 1) 18.52%, rgba(193, 164, 255, 1) 86.15%);
-  //     -webkit-background-clip: text;
-  //     -webkit-text-fill-color: transparent;
-  //   }
-  //   img {
-  //     display: inline-block;
-  //   }
-
-  //   .text {
-  //     display: inline-block;
-  //   }
-  // }
+  position: relative;
+  .box1{
+    margin-left: 14%;
+    transition:  margin-left 1s ;
+  }
+  img{
+    position: absolute;
+    right: 10px;
+    opacity: 0;
+    top:88px
+  }
+   &:hover {
+     border-top: 4px solid;
+     padding-top: 30px;
+     border-image-source: linear-gradient(90deg, rgba(246, 98, 249, 1) 9.5%, rgba(65, 19, 112, 0) 100%);
+     border-image-outset: 2px;
+     & span {
+       background: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(222, 20, 255, 0) 100%), linear-gradient(166.65deg, rgba(246, 98, 250, 1) 18.52%, rgba(193, 164, 255, 1) 86.15%);
+       -webkit-background-clip: text;
+       -webkit-text-fill-color: transparent;
+     }
+     img {
+       animation: imgChange 0.8s linear !important;
+       opacity: 1;
+     }
+     .text {
+       display: inline-block;
+     }
+     & .box1{
+       margin-left: 0px
+     }
+  
+   }
 `
 const Circle = styled.div`
   width: 20px;
@@ -131,25 +146,25 @@ const PieOption = {
 }
 const page2List = [
     {
-        img: '/images/svg/myStake.svg',
+        img: '/images/item1.png',
         num: '01',
         name: 'MarsNode',
         text: 'With a total supply of 30,000, MarsNode leverages the LMC economic model and user sociagraphs to foster Web3 interactions, enhancing point-to-multipoint engagement and drivingprofitability.'
     },
     {
-        img: '/images/svg/myStake.svg',
+        img: '/images/item2.png',
         num: '02',
         name: 'Stake',
         text: "Boost your profits with LittleMami’s easy and innovative staking, offering rewards and secure governance."
     },
     {
-        img: '/images/svg/myStake.svg',
+        img: '/images/item3.png',
         num: '03',
         name: 'LaunchPad',
         text: "Open doors to diverse earnings for users by supporting emerging projects with our extensive LaunchPad network."
     },
     {
-        img: '/images/svg/myStake.svg',
+        img: '/images/item4.png',
         num: '04',
         name: 'Loan',
         text: "By holding LittleMami assets and compatible tokens, users can engage in hassle-free loan services using NFTs as collateral without the need to sell, optimizing asset utilization."
@@ -161,24 +176,21 @@ const Page2 = (props) => {
             {page2List.map((item, index) => {
                 return (
                     <Page2Wrapper key={item.name} className="">
-                        <div className="fx jc-start" style={{marginLeft: '14%'}}>
+                        <div className="fx jc-start box1">
                             <span className='fz20 fw500'>/{item.num}</span>
-                            <div className="fx-row ai-ct">
-                                <div>
+                            <div className=""  style={{marginRight: '46px'}}>
                                     <div className='fz116' style={{marginTop: '-20px'}}>{item.name}</div>
                                     <div className='fz16 hidden text' style={{width: "820px"}}>
                                         {item.text}
                                     </div>
-                                </div>
-                                <Image
-                                    alt={item.name}
-                                    className='hidden'
-                                    src={item.img}
-                                    width="100"
-                                    height="100"
-                                />
                             </div>
                         </div>
+                        <Image
+                            alt={item.name}
+                            src={item.img}
+                            width={190}
+                            height={190}
+                        />
                     </Page2Wrapper>
                 );
             })}
