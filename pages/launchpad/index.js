58fa67b07ac739f3bc0417a0397495040558e7c3
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-07-21 11:47:56 +0400

    Dev (#33)
    Dev

--- pages/launchpad/index.js
+++ pages/launchpad/index.js
@@ -1,9 +1,9 @@
 
-import {  Container,  } from '@/components/LaunchpadLayout'
-import { styled } from 'styled-components'
-import Image from "next/image"
-import ETHIcon from '@/public/images/eth.png'
-import Airdrop from '@/public/images/airdrop.png'
+import { Container } from "@/components/LaunchpadLayout";
+import { styled } from "styled-components";
+import Image from "next/image";
+import ETHIcon from "@/public/images/eth.png";
+import Airdrop from "@/public/images/airdrop.png";
 
 import { useRouter } from "next/router";
 import StakeTitle from "@/components/Stake/StakeTitle";
