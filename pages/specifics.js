140b0e472c123228f49d27607940594ad7416bf9
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-07-22 17:21:25 +0400

    Update specifics.js

--- pages/specifics.js
+++ pages/specifics.js
@@ -79,23 +79,33 @@ const Mars = () => {
         functionName: "balanceOf",
         args: [marsContract?.address],
       },
+      {
+        address: lmc,
+        abi: LMCABI,
+        functionName: "balanceOf",
+        args: [nodeContract?.address],
+      },
     ],
   });
 
-  const balance = reads4?.[0]?.result;
-  console.log(balance);
+  const marsBalance = reads4?.[0]?.result;
+  const nodeBalance = reads4?.[1]?.result;
+
   return (
     <>
       <div className="text-center font-black text-5xl">
         <div className="border">
           <div>Mars</div>
-          <div>Total Stake LMC :{balance?.toString()}</div>
-          <div>Total Point :{totalPoint?.toString()}</div>
+          <div>质押lmc总量:{marsBalance?.toString()}</div>
+          <div>总分数 :{totalPoint?.toString()}</div>
         </div>
         <div className="border mt-10">
           <div>Node</div>
           <div>
-            Total Node Sell For LMC:
+            购买lmc总量:{((nodeBalance || 0n) / BigInt(1e18))?.toString()}
+          </div>
+          <div>
+            lmc卖的节点数量:
             {((totalSell || 0n) - 648n)?.toString()}
           </div>
         </div>
